b6753a5e5c0a387832b6c689ba5986927bde502e
Andrew Weymes <75763839+dimmerz92@users.noreply.github.com>
2024-08-04 10:52:54 +0000

    Issue 5 session logic (#6)
    * add session queries
    
    * add database initialiser
    
    * add session creation function
    
    * add session revoke function
    
    * add expiry to get session query
    
    * add validate session function
    
    * remove bool returns and add session checking middleware
    
    * add admin only middleware
    
    * add session check to router

--- go.mod
+++ go.mod
@@ -4,8 +4,10 @@ go 1.22.5
 
 require (
 	github.com/a-h/templ v0.2.747
+	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.12.0
+	github.com/mattn/go-sqlite3 v1.14.22
 )
 
 require (
---
+++ internal/auth/sessions.go
@@ -0,0 +1,84 @@
+package auth
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"the-iron-cygnet/database"
+	"the-iron-cygnet/database/sqlc"
+	"the-iron-cygnet/internal/utils"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+)
+
+func CreateSession(ctx echo.Context, userId string) error {
+	sessionId := uuid.New().String()
+	expiry := time.Now().Add(time.Hour)
+
+	if err := database.DB.Queries.CreateSession(ctx.Request().Context(), sqlc.CreateSessionParams{
+		ID:     sessionId,
+		Userid: userId,
+		Expiry: expiry.Unix(),
+	}); err != nil {
+		log.Println(err)
+		return fmt.Errorf(utils.ServerError)
+	}
+
+	ctx.SetCookie(&http.Cookie{
+		Name:     "session",
+		Value:    sessionId,
+		Expires:  expiry,
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteStrictMode,
+	})
+
+	return nil
+}
+
+func RevokeSession(ctx echo.Context) {
+	sessionCookie, err := ctx.Cookie("session")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	if err := database.DB.Queries.DeleteSession(ctx.Request().Context(), sessionCookie.Value); err != nil {
+		log.Println(err)
+		return
+	}
+
+	ctx.SetCookie(&http.Cookie{
+		Name:     "session",
+		Value:    "revoked",
+		MaxAge:   -1,
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteStrictMode,
+	})
+}
+
+func ValidateSession(ctx echo.Context) {
+	if err := database.DB.Queries.DeleteExpiredSessions(ctx.Request().Context()); err != nil {
+		log.Println(err)
+		return
+	}
+
+	sessionCookie, err := ctx.Cookie("session")
+	if err != nil {
+		return
+	}
+
+	user, err := database.DB.Queries.GetSession(ctx.Request().Context(), sessionCookie.Value)
+	if err != nil {
+		log.Println(err)
+		return
+	} else if user.Expiry <= time.Now().Unix() {
+		return
+	}
+
+	ctx.Set("UserID", user.Userid.String)
+	ctx.Set("Role", user.Role.String)
+}
