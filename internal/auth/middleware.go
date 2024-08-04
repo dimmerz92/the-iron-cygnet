3b50a23353229628670e96c152ad4726a2de5f58
Andrew Weymes <andrew.weymes@sittellalab.com.au>
2024-08-04 18:48:46 +0800

    add admin only middleware

--- internal/auth/middleware.go
+++ internal/auth/middleware.go
@@ -1,10 +1,26 @@
 package auth
 
-import "github.com/labstack/echo/v4"
+import (
+	"fmt"
+	"net/http"
+	"the-iron-cygnet/internal/utils"
+
+	"github.com/labstack/echo/v4"
+)
 
 func CheckSession(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(ctx echo.Context) error {
 		ValidateSession(ctx)
+		return next(ctx)
+	}
+}
+
+func AdminOnly(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(ctx echo.Context) error {
+		role, ok := ctx.Get("Role").(string)
+		if !ok || utils.Contains(role, []string{"admin", "judge"}) {
+			return ctx.String(http.StatusForbidden, fmt.Sprintf("%d FORBIDDEN", http.StatusForbidden))
+		}
 		return next(ctx)
 	}
 }
