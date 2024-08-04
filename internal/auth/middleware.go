6d090608efc37f7af371286696d0d4a073ee2ea5
Andrew Weymes <andrew.weymes@sittellalab.com.au>
2024-08-04 22:38:42 +0800

    fix admin only logic

--- internal/auth/middleware.go
+++ internal/auth/middleware.go
@@ -18,7 +18,7 @@ func CheckSession(next echo.HandlerFunc) echo.HandlerFunc {
 func AdminOnly(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(ctx echo.Context) error {
 		role, ok := ctx.Get("Role").(string)
-		if !ok || utils.Contains(role, []string{"admin", "judge"}) {
+		if !ok || !utils.Contains(role, []string{"admin", "judge"}) {
 			return ctx.String(http.StatusForbidden, fmt.Sprintf("%d FORBIDDEN", http.StatusForbidden))
 		}
 		return next(ctx)
