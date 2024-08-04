9e5afd1c7193a71c8d42b580eb893cd8f52dd7f8
Andrew Weymes <andrew.weymes@sittellalab.com.au>
2024-08-04 18:50:36 +0800

    add session check to router

--- internal/handlers/routes.go
+++ internal/handlers/routes.go
@@ -1,7 +1,12 @@
 package handlers
 
-import "github.com/labstack/echo/v4"
+import (
+	"the-iron-cygnet/internal/auth"
+
+	"github.com/labstack/echo/v4"
+)
 
 func InitHandlerRoutes(e *echo.Echo) {
+	e.Use(auth.CheckSession)
 	e.Static("/static", "static")
 }
