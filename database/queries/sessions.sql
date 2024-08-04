4775fc89dd3e13e96c1821d31bb36566afbff3ce
Andrew Weymes <andrew.weymes@sittellalab.com.au>
2024-08-04 18:01:25 +0800

    add session queries

---
+++ database/queries/sessions.sql
@@ -0,0 +1,29 @@
+-- name: CreateSession :exec
+INSERT INTO
+	sessions (id, userId, expiry)
+VALUES
+	(?, ?, ?);
+
+-- name: GetSession :one
+SELECT
+	s.id AS sessionId,
+	u.id AS userId,
+	r.name AS role
+FROM
+	sessions AS s
+	LEFT JOIN users AS u ON s.userId = u.id
+	LEFT JOIN roles AS r ON u.role = r.id
+WHERE
+	s.id = ?;
+
+-- name: DeleteSession :exec
+DELETE FROM
+	sessions
+WHERE
+	id = ?;
+
+-- name: DeleteExpiredSessions :exec
+DELETE FROM
+	sessions
+WHERE
+	expiry <= strftime('%s', 'now');
